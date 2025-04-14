bc06aa9e0cffe72f2ed8ba3b1d6a020f84513e8c
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-04-14 11:34:42 +0330

    4-random-number-genrator

--- 4-random-number-generator/script.js
+++ 4-random-number-generator/script.js
@@ -1,1 +1,6 @@
-
+let label = document.getElementById("label");
+document.getElementById("generate").onclick = function(){
+    let randomNumber =Math.round(Math.random()*10);
+    console.log(randomNumber);
+    label.textContent = randomNumber;
+ }
