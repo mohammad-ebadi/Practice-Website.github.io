9627905a37370af11e3d77e43fc193e937aeefc6
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-05-28 21:37:21 +0330

    1-card

--- REACT/1-card/vite-project/src/Test.jsx
+++ REACT/1-card/vite-project/src/Test.jsx
@@ -3,16 +3,22 @@ import { useEffect, useState } from "react";
 
 export default function Test() {
   const [catFact, setCatFact] = useState("");
+
   useEffect(() => {
+    fetchFact();
+  }, []);
+
+  const fetchFact = () => {
     // API
     Axios.get("https://catfact.ninja/fact").then((res) => {
       setCatFact(res.data.fact);
     });
-  }, []);
+  };
 
   return (
     <>
       <h1>Fetching data from an API (useEffect)</h1>
+      <button onClick={fetchFact}>Fetch Data</button>
       <p>{catFact}</p>
     </>
   );
